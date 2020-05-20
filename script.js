318db07855155cdf567ba20fc06dc1fc81afeebe
mitch9256 <mlong2023@hightechhigh.org>
2020-05-20 21:43:30 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    createPage();
+    close();
+    open()
+});
+
+function createPage() {
+    var max = 0;
+    var i;
+    for (i = 0; i > max; m++) {
+
+    }
+}
+
+function createElement() {
+
+}
+
+function close() {
+    $('.popup').click(function () {
+        $(this).css('display', 'none')
+    });
+}
+
+function open() {
+    $('.open-box').click(function () {
+        $($(this).closet('.popup')).css('display', 'block')
+    });
+}
